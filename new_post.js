59b5f1e47851a6df250884eeb7a669471770f1b2
vedanshijain <vedanshi.jain@indiamart.com>
2023-06-12 17:28:44 +0530

    recom cats

--- new_post.js
+++ new_post.js
@@ -38,3 +38,790 @@ function UpdateChatName(tmpId) {
     $("#t" + tmpId + "_hiid").text("Welcome");
   }
 }
+
+
+FormSeq.prototype.BLChat = function (tmpId) {
+  UpdateChatName(tmpId);
+  ReqObj.Form[tmpId].IsqStep1 = ReturnCorrectVal(
+    ReqObj.Form[tmpId].IsqStep1,
+    1
+  );
+  ReqObj.Form[tmpId].IsqStepN = ReturnCorrectVal(
+    ReqObj.Form[tmpId].IsqStepN,
+    1
+  );
+  ReqObj.Form[tmpId].isProdNameQuesShown = false;
+  ReqObj.Form[tmpId].isOtpShownOnFirstStep = false;
+  ReqObj.Form[tmpId].toCloseChatBL = false;
+  this.BLChatQuestionStart(tmpId); //
+};
+FormSeq.prototype.BLChatQuestionStart = function (tmpId) {
+  isChatBlSequenceUpdated = false;
+  HideTncBox(tmpId); // new chat bl unidentified
+  removechatblerror(tmpId);
+  if (imeshExist() !== "") $("#t" + tmpId + "mflag").css("display", "none");
+  $("#t" + tmpId + "mflag").remove();
+  this.BLChatEvents(tmpId);
+  if (
+    imeshExist() !== "" &&
+    isSecondBl() &&
+    usercookie.getParameterValue(imeshExist(), "uv") !== "V" &&
+    currentISO() === "IN"
+  ) {
+    ReqObj.Form[tmpId].isOtpShownOnFirstStep = true;
+    ReqObj.Form[tmpId].isProdNameQuesShown = false;
+    ReqObj.Form[tmpId].isProdNameShown = false;
+    this.BLChatOtp(tmpId);
+  } else {
+    ReqObj.Form[tmpId].isOtpShown = false;
+    ReqObj.Form[tmpId].isProdNameQuesShown = false;
+    ReqObj.Form[tmpId].isProdNameShown = false;
+      var checkisq = isSet(ReqObj.Form[tmpId].IsqLength) && ReqObj.Form[tmpId].IsqLength != 0;
+      if (IsChatBLInline(tmpId) && !imeshExist()) {   //new chat bl unidentified
+        ReqObj.Form[tmpId].isqAsked = true;    
+              
+        if (ShowProdName(tmpId) && !ReqObj.Form[tmpId].isProdNameShown) {
+          this.BLChatProdName(tmpId);
+        }
+        else if(checkisq){
+        this.BLChatIsq(tmpId);
+        }
+        else
+        this.BLChatLogin(tmpId);
+  }
+      else
+        this.BLChatLogin(tmpId);
+  }
+};
+
+FormSeq.prototype.BLChatProdName = function (tmpId) {
+  var that = this;
+  this.NumberofClassCalled = 1;
+  ReqObj.Form[tmpId].isProdNameShown = true;
+  if(IsChatBLInline(tmpId) && !imeshExist())   //new chat bl unidentified
+  {
+    var hooks = {
+      pre: [],
+      post: [this.BLChatIsq],
+      current: [this.BLChatProdName],
+    };
+  }
+  else{
+    var hooks = {
+      pre: [],
+      post: [this.BLChatLogin],
+      current: [this.BLChatProdName],
+    };
+  }
+  var array = {
+    UiArray: [],
+    ServiceArray: [],
+  };
+  if (
+    !(checkblockedUser() && im_issExist() === "") &&
+    (ReqObj.Form[tmpId].UserInputs["IsProduct"] ===
+      "Change the product".trim() ||
+      (isSet(ReqObj.Form[tmpId].isproductshown) &&
+        ReqObj.Form[tmpId].isproductshown))
+  ) {
+    var ProdNameObject = {
+      object: {
+        obj: new ProductName(tmpId),
+        toReplace: true,
+        isService: false,
+        array: array,
+        hooks: hooks,
+      },
+      tmpId: tmpId,
+      that: that,
+      AfterService: [],
+      hasFallback: false,
+      FallbackObj: null,
+    };
+    CreateSeq(ProdNameObject);
+    if (imeshExist()) {
+      if (
+        currentISO() === "IN" ||
+        (currentISO() !== "IN" &&
+          usercookie.getParameterValue(
+            usercookie.getCookie("ImeshVisitor"),
+            "fn"
+          ))
+      ) {
+        hooks = {
+          pre: [],
+          post: [],
+          current: [],
+        };
+        var GenObject = returnGenObject(tmpId, array, hooks, that, 0);
+        this.MakeSeq(GenObject);
+      }
+    }
+  } else {
+    //this.BLChatCountry(tmpId);
+    if (checkblockedUser()) {
+      this.BLChatOtp(tmpId);
+      ReqObj.Form[tmpId].isProdNameShown = false;
+    } else this.BLChatLogin(tmpId);
+  }
+};
+
+FormSeq.prototype.BLChatLogin = function (tmpId) {
+  var that = this;
+  this.NumberofClassCalled = 1;
+  ShowHideTNC(tmpId); // new chat bl unidentified
+  if (!imeshExist()) {
+      if (ShowProdName(tmpId) && !ReqObj.Form[tmpId].isProdNameShown) {  
+      var hooks = {
+        pre: [],
+        post: [this.BLChatProdName],
+        current: [this.BLChatLogin],
+      };
+    } else {
+      var hooks = {
+        pre: [],
+        post: [this.BLChatName],
+        current: [this.BLChatQuestionStart],
+      };
+    }
+    var array = {
+      UiArray: [],
+      ServiceArray: [],
+    };
+
+    var UserLoginObject = {
+      object: {
+        obj: new UserLogin(tmpId),
+        toReplace: true,
+        isService: false,
+        array: array,
+        hooks: hooks,
+      },
+      tmpId: tmpId,
+      that: that,
+      AfterService: [],
+      hasFallback: false,
+      FallbackObj: null,
+    };
+    CreateSeq(UserLoginObject);
+    if (currentISO() === "IN") {
+      hooks = {
+        pre: [],
+        post: [],
+      };
+      var GenObject = {
+        object: {
+          obj: new Generation(0),
+          toReplace: true,
+          isService: true,
+          array: array,
+          hooks: hooks,
+        },
+        tmpId: tmpId,
+        that: that,
+        AfterService: [],
+        hasFallback: false,
+        FallbackObj: null,
+      };
+      this.MakeSeq(GenObject);
+    }
+  } else {
+    if (ShowProdName(tmpId) && !ReqObj.Form[tmpId].isProdNameShown) {
+      this.BLChatProdName(tmpId);
+    } else this.BLChatName(tmpId);
+  }
+};
+
+FormSeq.prototype.BLChatName = function (tmpId) {
+  ShowHideTNC(tmpId); // new chat bl unidentified
+  var that = this;
+  this.NumberofClassCalled = 1;
+  if (!(checkblockedUser() && im_issExist() === "") && !(notEmpty(usercookie.getParameterValue(imeshExist(), "fn")) || (isSet(ReqObj.UserDetail) && notEmpty(ReqObj.UserDetail.fn && isSet(ReqObj.UserDetail.fn))))) {
+    var array = {
+      UiArray: [],
+      ServiceArray: [],
+    };
+    var hooks = {
+      pre: [this.BLChatEmail],
+      post: [],
+      current: [this.BLChatName],
+    };
+    if (CallGeneration(tmpId)) {
+      var GenObject = returnGenObject(tmpId, array, hooks, that, 0);
+      this.MakeSeq(GenObject);
+      hooks = {
+        pre: [],
+        post: [],
+        current: [],
+      };
+    }
+
+    var PostBlEnqUpdateObj = returnPostBlEnqObject(tmpId, array, hooks, that, "");
+    var NECObj = {
+      object: {
+        obj: new ContactDetail(1, 0, 0),
+        toReplace: false,
+        isService: false,
+        array: array,
+        hooks: hooks,
+      },
+      tmpId: tmpId,
+      that: that,
+      AfterService: [PostBlEnqUpdateObj],
+      hasFallback: false,
+      FallbackObj: null,
+    };
+    CreateSeq(NECObj);
+    //is not indian or offer id not generated
+  } else {
+    if (checkblockedUser()) this.BLChatOtp(tmpId);
+    else this.BLChatEmail(tmpId);
+  }
+};
+
+FormSeq.prototype.BLChatCity = function (tmpId) {
+  var that = this;
+  this.NumberofClassCalled = 1;
+  var hooks = {
+    pre: [this.BLChatOtp],
+    post: [],
+    current: [this.BLChatCity],
+  };
+  var array = {
+    UiArray: [],
+    ServiceArray: [],
+  };
+  if (CallGeneration(tmpId)) {
+    hooks = {
+      pre: [],
+      post: [],
+    };
+    var GenObject = returnGenObject(tmpId, array, hooks, that, 0);
+    this.MakeSeq(GenObject);
+    hooks = {
+      pre: [],
+      post: [],
+      current: [],
+    };
+  }
+  if (!usercookie.getParameterValue(imeshExist(), "ctid") && currentISO() === "IN") {
+    //if offer id generated
+    var PostBlEnqUpdateObj = returnPostBlEnqObject(tmpId, array, hooks, that, "");
+
+    hooks = {
+      pre: [this.BLChatOtp],
+      post: [],
+      current: [this.BLChatCity],
+    };
+
+    var NECObj = {
+      object: {
+        obj: new ContactDetail(0, 0, 1),
+        toReplace: false,
+        isService: false,
+        array: array,
+        hooks: hooks,
+      },
+      tmpId: tmpId,
+      that: that,
+      AfterService: [PostBlEnqUpdateObj],
+      hasFallback: false,
+      FallbackObj: null,
+    };
+    CreateSeq(NECObj);
+
+    //check here is offerid is generated
+  } else {
+    if (CallGeneration(tmpId)) {
+      // if (currentISO() === "IN") {
+      //   this.BLChatIsq(tmpId);
+      // } else {
+      //   //show static question here
+      //   this.BLChatStaticQuestion(tmpId);
+      // }
+      this.BLChatIsq(tmpId);
+    } else {
+      this.BLChatOtp(tmpId);
+    }
+  }
+};
+FormSeq.prototype.BLChatEmail = function (tmpId) {
+  var that = this;
+  this.NumberofClassCalled = 1;
+  var hooks = {
+    pre: [this.BLChatCity],
+    post: [],
+    current: [this.BLChatEmail],
+  };
+  var array = {
+    UiArray: [],
+    ServiceArray: [],
+  };
+  if (CallGeneration(tmpId)) {
+    hooks = {
+      pre: [],
+      post: [],
+    };
+    var GenObject = returnGenObject(tmpId, array, hooks, that, 0);
+    this.MakeSeq(GenObject);
+    hooks = {
+      pre: [],
+      post: [],
+      current: [],
+    };
+  }
+  if (ShowEmail() || ShowMobile()) {
+    //if offer id generated
+    var PostBlEnqUpdateObj = returnPostBlEnqObject(
+      tmpId,
+      array,
+      hooks,
+      that,
+      ""
+    );
+
+    hooks = {
+      pre: [this.BLChatCity],
+      post: [],
+      current: [this.BLChatEmail],
+    };
+
+    var NECObj = {
+      object: {
+        obj: new ContactDetail(0, 1, 0),
+        toReplace: false,
+        isService: false,
+        array: array,
+        hooks: hooks,
+      },
+      tmpId: tmpId,
+      that: that,
+      AfterService: [PostBlEnqUpdateObj],
+      hasFallback: false,
+      FallbackObj: null,
+    };
+    CreateSeq(NECObj);
+
+    //check here is offerid is generated
+  } else {
+    this.BLChatCity(tmpId);
+  }
+};
+
+
+FormSeq.prototype.BLChatOtp = function (tmpId) {
+  var array = {
+    UiArray: [],
+    ServiceArray: [],
+  };
+  var hooks = {
+    pre: [],
+    post: [this.BLChatIsq],
+    current: [this.BLChatQuestionStart],
+  };
+  var messageObj = {
+    message: "",
+  };
+  if (currentISO() === "IN") {
+    if (checkblockedUser()) {
+      hooks = {
+        pre: [],
+        post: [this.BLChatLogin],
+        current: [this.BLChatOtp],
+      };
+    } else if (
+      isSecondBl() &&
+      usercookie.getParameterValue(imeshExist(), "uv") !== "V" &&
+      ReqObj.Form[tmpId].isOtpShownOnFirstStep
+    ) {
+      if (ShowProdName(tmpId) && !ReqObj.Form[tmpId].isProdNameShown) {
+        hooks = {
+          pre: [],
+          post: [this.BLChatProdName],
+          current: [this.BLChatOtp],
+        };
+      } else {
+        hooks = {
+          pre: [],
+          post: [this.BLChatName],
+          current: [this.BLChatOtp],
+        };
+      }
+      messageObj.message = "second";
+    } else {
+      if (ShowProdName(tmpId) && !ReqObj.Form[tmpId].isProdNameShown) {
+        hooks = {
+          pre: [],
+          post: [this.BLChatProdName],
+
+          current: [this.BLChatQuestionStart],
+        };
+      } else {
+        hooks = {
+          pre: [],
+          post: [this.BLChatIsq],
+          current: [this.BLChatQuestionStart],
+        };
+      }
+      messageObj.message = "";
+    }
+  }
+
+  var that = this;
+  this.NumberofClassCalled = 1;
+
+  if (ShowOtp() || (checkblockedUser() && im_issExist() === "")) {
+    var UserVerificationObj = {
+      object: {
+        obj: new UserVerification(tmpId),
+        toReplace: false,
+        isService: false,
+        array: array,
+        hooks: hooks,
+      },
+      tmpId: tmpId,
+      that: that,
+      AfterService: [],
+      hasFallback: false,
+      FallbackObj: null,
+      message: messageObj,
+    };
+    CreateSeq(UserVerificationObj);
+    if (
+      (ReqObj.Form[tmpId].isOtpShownOnFirstStep && imeshExist()) ||
+      checkblockedUser()
+    ) {
+      if (
+        checkblockedUser() ||
+        currentISO() === "IN" ||
+        (currentISO() !== "IN" &&
+          usercookie.getParameterValue(imeshExist(), "fn"))
+      ) {
+        hooks = {
+          pre: [],
+          post: [],
+          current: [],
+        };
+        var GenObject = returnGenObject(tmpId, array, hooks, that, 0);
+        this.MakeSeq(GenObject);
+      }
+    }
+  } else {
+    this.BLChatIsq(tmpId);
+  }
+};
+
+FormSeq.prototype.BLChatIsq = function (tmpId) {
+  var that = this;
+  this.NumberofClassCalled = 1;
+
+  var array = {
+    UiArray: [],
+    ServiceArray: [],
+  };
+  var hooks = {
+    pre: [this.BLChatIsqSubsequent],
+    post: [],
+    current: [this.BLChatIsq],
+  };
+  //if otp shown
+
+  //else
+  // var hooks = {
+  //   pre: [this.BLChatOtp],
+  //   post: []
+  // };
+
+  //check here is offerid is generated and also if it is not in hit array
+  if (CallGeneration(tmpId)) {
+    hooks = {
+      pre: [this.BLChatOtp],
+      post: [],
+      current: [this.BLChatIsq],
+    };
+    var GenObject = returnGenObject(tmpId, array, hooks, that, 0);
+    this.MakeSeq(GenObject);
+    hooks = {
+      pre: [],
+      post: [],
+      current: [this.BLChatIsq],
+    };
+  }
+
+  if (ShowIsq(tmpId)) {
+    if (ShowReqBox(tmpId)) {
+      var IsqFallback = {
+        object: {
+          obj: new RequirementDtl(tmpId),
+          toReplace: true,
+          isService: false,
+          array: array,
+          hooks: hooks,
+          countlastUpdated: false,
+        },
+        tmpId: tmpId,
+        that: that,
+        AfterService: [],
+        hasFallback: false,
+        FallbackObj: null,
+      };
+    } else {
+      //if otp shown
+      var IsqFallback = {
+        object: {
+          obj: new ThankYou(tmpId),
+          toReplace: false,
+          objHtmlId: tmpId + "_thankDiv",
+          isService: false,
+          array: array,
+          hooks: hooks,
+        },
+        tmpId: tmpId,
+        that: that,
+        AfterService: [],
+        hasFallback: false,
+        FallbackObj: null,
+      };
+      //else have otp as fallback
+    }
+    var IsqObject = {
+      object: {
+        obj: ReqObj.Form[tmpId].GlobalIsqObject,
+        array: array,
+        hooks: hooks,
+        isService: false,
+      },
+      tmpId: tmpId,
+      that: that,
+      AfterService: [],
+      hasFallback: true,
+      FallbackObj: IsqFallback,
+    };
+    CreateSeq(IsqObject);
+  } else{    // new chat bl unidentified
+    if(!imeshExist())
+    this.BLChatLogin(tmpId);
+    else
+    this.BLChatRd(tmpId);
+  }
+    // this.BLChatRd(tmpId);
+};
+
+FormSeq.prototype.BLChatIsqSubsequent = function (tmpId) {
+  var that = this;
+  this.NumberofClassCalled = 1;
+  // ReqObj.Form[tmpId].flags["IsFirstStep"] = false;
+  var array = {
+    UiArray: [],
+    ServiceArray: [],
+  };
+
+  //if otp shown
+  var hooks = {
+    pre: [this.BLChatIsqSubsequent],
+    post: [],
+    current: [this.BLChatIsqSubsequent],
+  };
+
+  if (
+    ReqObj.Form[tmpId].IsqArray === "" ||
+    (currentISO() === "IN" &&
+      ReqObj.Form[tmpId].prevCount < ReqObj.Form[tmpId].IsqLength) ||
+    (currentISO() !== "IN" &&
+      ReqObj.Form[tmpId].prevCount < ReqObj.Form[tmpId].IsqLength &&
+      ReqObj.Form[tmpId].Isq.HasHtmlCalled !== true)
+  ) {
+    if (!ReqObj.Form[tmpId].flags.isDescDivShown) {
+      var IsqFallback = {
+        object: {
+          obj: new RequirementDtl(tmpId),
+          toReplace: true,
+          isService: false,
+          array: array,
+          hooks: hooks,
+          countlastUpdated: false,
+        },
+        tmpId: tmpId,
+        that: that,
+        AfterService: [],
+        hasFallback: false,
+        FallbackObj: null,
+      };
+    } else {
+      //if otp shown
+      var IsqFallback = {
+        object: {
+          obj: new ThankYou(tmpId),
+          toReplace: false,
+          isService: false,
+          array: array,
+          hooks: hooks,
+        },
+        tmpId: tmpId,
+        that: that,
+        AfterService: [],
+        hasFallback: false,
+        FallbackObj: null,
+      };
+      //else have otp as fallback
+    }
+    var IsqObject = {
+      object: {
+        obj: ReqObj.Form[tmpId].GlobalIsqObject,
+        array: array,
+        hooks: hooks,
+        isService: false,
+      },
+      tmpId: tmpId,
+      that: that,
+      AfterService: [],
+      hasFallback: true,
+      FallbackObj: IsqFallback,
+    };
+    CreateSeq(IsqObject);
+  } else{    // new chat bl unidentified
+    if(!imeshExist())
+    this.BLChatLogin(tmpId);
+    else
+    this.BLChatRd(tmpId);
+  }
+    // this.BLChatRd(tmpId);
+};
+
+FormSeq.prototype.BLChatStaticQuestion = function (tmpId) {
+  var that = this;
+  this.NumberofClassCalled = 1;
+
+  var array = {
+    UiArray: [],
+    ServiceArray: [],
+  };
+  var hooks = {
+    pre: [this.BLChatRd],
+    post: [],
+    current: [this.BLChatStaticQuestion],
+  };
+  if (
+    returnIsEnrichShownKeyVal(tmpId) &&
+    ReqObj.Form[tmpId].modrefType === "product"
+  ) {
+    var StaticQues = {
+      object: {
+        obj: new BlStaticQues(tmpId),
+        toReplace: true,
+        isService: false,
+        array: array,
+        hooks: hooks,
+      },
+      tmpId: tmpId,
+      that: that,
+      AfterService: [],
+      hasFallback: false,
+      FallbackObj: null,
+    };
+    CreateSeq(StaticQues);
+  } else {
+    this.BLChatRd(tmpId);
+  }
+};
+
+FormSeq.prototype.BLChatRd = function (tmpId) {
+  var that = this;
+  this.NumberofClassCalled = 1;
+  if(ReqObj.Form[tmpId].isqAsked == true && !ReqObj.Form[tmpId].saveOnce) { //new chat bl unidentified
+  ReqObj.Form[tmpId].GlobalIsqObject.onSubmit(tmpId);
+  ReqObj.Form[tmpId].saveOnce = true;
+  }
+  var array = {
+    UiArray: [],
+    ServiceArray: [],
+  };
+  var hooks = {
+    pre: [this.BlChatCName],
+    post: [],
+    current: [this.BLChatRd],
+  };
+  var md = toAskMoreDetails(tmpId);
+  if (
+    md.ask === false &&
+    usercookie.getParameterValue(imeshExist(), "uv") !== "V" &&
+    currentISO() === "IN"
+  ) {
+    var hooks = {
+      pre: [this.BLChatOtp],
+      post: [],
+      current: [this.BLChatRd],
+    };
+  }
+  if (CallGeneration(tmpId)) {
+    hooks = {
+      pre: [this.BlChatCName],
+      post: [],
+      current: [this.BLChatRd],
+    };
+    var GenObject = returnGenObject(tmpId, array, hooks, that, 0);
+    this.MakeSeq(GenObject);
+    hooks = {
+      pre: [],
+      post: [],
+      current: [this.BLChatRd],
+    };
+  }
+
+  if (ShowReqBox(tmpId)) {
+    var RdObj = {
+      object: {
+        obj: new RequirementDtl(tmpId),
+        toReplace: true,
+        isService: false,
+        array: array,
+        hooks: hooks,
+        countlastUpdated: false,
+      },
+      tmpId: tmpId,
+      that: that,
+      AfterService: [],
+      hasFallback: false,
+      FallbackObj: null,
+    };
+    CreateSeq(RdObj);
+  } else {
+    if (md.ask == true && md.key === 0) {
+      this.BlChatCName(tmpId);
+    } else if (EmailAfterReqbox(tmpId)) {
+      ReqObj.Form[tmpId].emailAfterReqbox = false;
+      this.BLChatThankYou(tmpId);
+    } else {
+      ReqObj.Form[tmpId].emailAfterReqbox = true;
+      this.BLChatEmail(tmpId);
+    }
+  }
+};
+
+FormSeq.prototype.BlChatCName = function (tmpId) {
+  var that = this;
+  var array = {
+    UiArray: [],
+    ServiceArray: [],
+  };
+  // var hooks = {
+  //   pre: [this.BLChatThankYou],
+  //   post: [],
+  //   current: [this.BlChatCName]
+  // };
+  // if (usercookie.getParameterValue(imeshExist(), "uv") !== "V") {
+  var md = toAskMoreDetails(tmpId);
+  var hooks = {
+    pre: [this.BlChatgst],
+    post: [],
+    current: [this.BlChatCName],
+  };
+  // }
+  if (md.ask === true && md.key === 0) {
+    this.NumberofClassCalled = 1;
+    var mdObj = returnmdtlObject(array, hooks, tmpId, that, false, md);
+    CreateSeq(mdObj);
+  } else {
+    this.BlChatgst(tmpId);
+  }
+};
